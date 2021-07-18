c891a0b0fb5da4203e7e4b16196c8d9d5048d54d
Dulanga <dulangakithmini@gmail.com>
2021-07-18 15:24:13 +0530

    Create routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,10 @@
+import {Express} from "express";
+import * as bookController from "../controllers/bookController";
+
+export default function (app: Express) {
+    app.get("/books", bookController.getAllBooks);
+    app.get("/book/:id", bookController.getBook);
+    app.post("/book", bookController.addBook);
+    app.put("/book/:id", bookController.updateBook);
+    app.delete("/book/:id", bookController.deleteBook);
+}
