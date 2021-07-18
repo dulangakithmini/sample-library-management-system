3e12ddb1b46ad8f3c4951b78e02070aea65681d9
Dulanga <dulangakithmini@gmail.com>
2021-07-18 14:21:51 +0530

    Create getBook function in bookController

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,13 +1,24 @@
 import {Request, Response} from "express";
-import Book from "../models/bookModel";
+import Book, {IBook} from "../models/bookModel";
 
 // get all books
 export let getAllBooks = (req: Request, res: Response) => {
     let books = Book.find((err, books) => {
         if (err) {
-            res.send("Error!");
+            res.send(err);
         } else {
             res.send(books);
+        }
+    })
+}
+
+// get book by id
+export let getBook = (req: Request, res: Response) => {
+    let book = Book.findById(req.params.id, (err: any, book: IBook) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(book);
         }
     })
 }
