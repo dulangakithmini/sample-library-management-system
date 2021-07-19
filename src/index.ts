7bcdf967d35a5bc7bea8950109a6d8323819debd
Dulanga <dulangakithmini@gmail.com>
2021-07-19 20:13:28 +0530

    Change port to 3000 in master branch

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ app.get('/', (req, res) => {
     res.send('Testing...!');
 })
 
-app.listen(3003, () => {
-    console.log('The application is listening on port 3003!');
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
 
     routes(app);
 });
