5ae16a55835ad85f9a8b7ec3b71311c589b9dbbc
Dulanga <dulangakithmini@gmail.com>
2021-07-21 22:26:57 +0530

    Verify that users with the same email cannot be created

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,20 +4,27 @@ import * as bcrypt from 'bcrypt';
 
 // add user
 export let createUser = (req: Request, res: Response) => {
-    bcrypt.hash(req.body.password, 10, function (err, hash) {
-        if (err) {
-            console.log(err);
+    UserModel.find({email: req.body.email}).then(user => {
+        // to verify that the user is not an empty array
+        if (user.length >= 1) {
+            res.send("User already exists!");
         } else {
-            let user = new UserModel({
-                    email: req.body.email,
-                    password: hash,
+            bcrypt.hash(req.body.password, 10, function (err, hash) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let user = new UserModel({
+                        email: req.body.email,
+                            password: hash,
+                        }
+                    );
+                    user.save().then(result => {
+                        res.send("User created");
+                    }).catch(err => {
+                        console.log(err);
+                    });
                 }
-            );
-            user.save().then(result => {
-                res.send("User created");
-            }).catch(err => {
-                console.log(err);
             });
         }
-    });
+    })
 };
