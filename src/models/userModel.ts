49a3b028a4395bc600ead9718105500cc7959187
Dulanga <dulangakithmini@gmail.com>
2021-07-22 23:29:52 +0530

    Add role to User model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -12,6 +12,11 @@ const UserSchema = new Schema<IUser>({
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     password: {type: String, required: true, unique: true},
+    role: {
+        type: String,
+        default: "user",
+        enum: ["admin, user"],
+    }
 });
 
 const UserModel = model<IUser>("User", UserSchema);
