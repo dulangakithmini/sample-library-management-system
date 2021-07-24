3bb3fe02e02d3c06240f00f08d315c3022006cc7
Dulanga <dulangakithmini@gmail.com>
2021-07-24 22:29:37 +0530

    clean routes file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,33 +8,47 @@ import verifyRole from "../middleware/auth-role";
 import limitRequests from "../middleware/api-throttle";
 
 export default function (app: Express) {
+    // book routes
     app.get("/allBooks", checkAuth, verifyRole, bookController.getAllBooks);
     app.get("/book/:id", checkAuth, limitRequests, bookController.getBook);
     app.post("/book", checkAuth, verifyRole, bookController.addBook);
     app.put("/book/:id", checkAuth, verifyRole, bookController.updateBook);
     app.delete("/book/:id", checkAuth, verifyRole, bookController.deleteBook);
 
+    // book filter routes
     app.get("/booksByTitle/:title", checkAuth, limitRequests, bookController.getBooksByTitle);
     app.get("/booksByAuthor/:author", checkAuth, limitRequests, bookController.getBooksByAuthor);
 
-    app.get("/authors", checkAuth, verifyRole, authorController.getAllAuthors);
+    // book borrow and book
+    app.put("/books/book/:id", checkAuth, limitRequests, bookController.bookABook);
+    app.put("/books/borrow/:id", checkAuth, verifyRole, bookController.borrow);
 
+    // get available books
+    app.get("/books", checkAuth, limitRequests, bookController.getAvailableBooks);
+
+    // get booked and borrowed books - user
+    app.get("/bookedBooks", checkAuth, limitRequests, bookController.getBookedBooks);
+    app.get("/borrowedBooks", checkAuth, limitRequests, bookController.getBorrowedBooks);
+
+    // get all booked and borrowed books - admin
+    app.get("/bookedOrBorrowed", checkAuth, verifyRole, bookController.getBookedOrBorrowedBooks);
+
+    // upload book file and add
+    app.post("/upload", checkAuth, verifyRole, upload.single('bookList'), bookController.uploadBooks);
+    app.post("/bookList", checkAuth, verifyRole, bookController.addBookList);
+
+
+    // user routes
     app.post("/user/signup", userController.createUser);
     app.delete("/user/:id", checkAuth, verifyRole, userController.deleteUser);
     app.post("/user/login", userController.login);
 
 
-    app.put("/books/book/:id", checkAuth, limitRequests, bookController.bookABook);
-    app.put("/books/borrow/:id", checkAuth, verifyRole, bookController.borrow);
-
-    app.get("/books", checkAuth, limitRequests, limitRequests, bookController.getAvailableBooks);
-
-    app.get("/bookedBooks", checkAuth, limitRequests, bookController.getBookedBooks);
-    app.get("/borrowedBooks", checkAuth, limitRequests, bookController.getBorrowedBooks);
-
-    app.get("/bookedOrBorrowed", checkAuth, verifyRole, bookController.getBookedOrBorrowedBooks);
-
-    app.post("/upload", checkAuth, verifyRole, upload.single('bookList'), bookController.uploadBooks);
-    app.post("/bookList", checkAuth, verifyRole, bookController.addBookList);
-
+    // author routes
+    app.get("/authors", checkAuth, verifyRole, authorController.getAllAuthors);
+    app.get("/author/:id", checkAuth, verifyRole, authorController.getAuthor);
+    app.delete("/author/:id", checkAuth, verifyRole, authorController.deleteAuthor);
+    app.put("/author/:id", checkAuth, verifyRole, authorController.updateAuthor);
+    app.post("/author", checkAuth, verifyRole, authorController.addAuthor);
+    app.get("/authorByName/:name", checkAuth, verifyRole, authorController.getAuthorByName);
 }
